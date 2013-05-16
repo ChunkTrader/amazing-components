14afe472bf768f25ee9dd2371236ee509fcf63ed
Chunk <chunk.jumper@gmail.com>
2013-05-16 21:32:02 +0200

    PASO 2, falta añadir a base de datos

--- comprar.php
+++ comprar.php
@@ -59,8 +59,79 @@ switch ($regMem->getValor('accion')) {
 
 		// Actualizamos el carrito
 		$regSistema->setValor('carrito', $carrito);
-		break;
-}
+		break; // Guardar cambios
+
+	case 'Continuar':
+		$correcto = TRUE;
+		
+
+
+		// Validamos los datos del paso 2
+		$regexp = "/[\^<,\"@\/\{\}\(\)\*\$%\?=>:\|;#]+/i";
+		if (preg_match($regexp, $regMem->getValor('nombre'))) {
+		    // error
+		    $regError->setError('nombre', 'El nombre contiene carácteres no válidos');
+		    $correcto = FALSE;
+		} else if (strlen($regMem->getValor('nombre'))<4) {
+			$regError->setError('nombre', 'El nombre debe tener al menos 4 caracteres(tiene'. strlen($regMem->getValor('nombre')) . ')');
+		    $correcto = FALSE;
+		}
+
+		if (preg_match($regexp, $regMem->getValor('apellido'))) {
+		$regError->setError('apellido', 'El apellido contiene carácteres no válidos');
+		    $correcto = FALSE;
+		} else if (strlen($regMem->getValor('apellido'))<4) {
+			$regError->setError('apellido', 'El apellido debe tener al menos 4 caracteres (tiene '. strlen($regMem->getValor('apellido')) . ')');
+		    $correcto = FALSE;
+		}
+
+		if (preg_match($regexp, $regMem->getValor('poblacion'))) {
+			$regError->setError('poblacion', 'La población contiene carácteres no válidos');
+		    $correcto = FALSE;
+		} else if (!$regMem->getValor('poblacion')) {
+			$regError->setError('poblacion', 'La población no puede estar en blanco.');
+		    $correcto = FALSE;
+		}
+
+		$regexp = "/^\d{5}$/";
+		if (!preg_match($regexp, $regMem->getValor('cp'))) {
+			$regError->setError('cp', 'El código postal debe estar compuesto por 5 dígitos');
+			$correcto = FALSE;
+		}
+
+		// La dirección no la validamos.
+		if (!$regMem->getValor('direccion')) {
+			$regError->setError('direccion', 'La direccion no puede estar en blanco.');
+		    $correcto = FALSE;
+		}
+
+		// Si todo es correcto preparamos el pedido y pasamos al siguiente paso
+		if ($correcto) {
+			$a = array(
+				'nombre' => $regMem->getValor('nombre'),
+				'apellido' => $regMem->getValor('apellido'),
+				'poblacion' => $regMem->getValor('poblacion'),
+				'direccion' => $regMem->getValor('direccion'),
+				'cp' => $regMem->getValor('cp'),
+				);
+			$regSistema->setValor('datos_envio', $a);
+			
+			// Si no existe dirección de envio para el usuario añadimos una nueva
+			// Si existe la actualizamos
+			// OJO, no comprobamos si los datos han cambiado.
+
+
+
+			$regMem->setValor('paso', 3);
+			$regFeedback->addFeedback('Datos de usuario correctos.');
+		}
+		
+
+		break; // Continuar
+
+
+
+} // Fin switch accion
 
 
 
@@ -70,10 +141,30 @@ if (!$carrito){
 	$regError->setError('general', 'El carrito está vacio.');
 }
 
+// Asignamos el subtitulo y acciones especiales según el paso
+switch ($regMem->getValor('paso')){
+	case 1:
+		$regMem->setValor('subtitulo', 'Paso 1: Comprobar el pedido');
+		break;
+	
+	case 2:
+		$regMem->setValor('subtitulo', 'Paso 2: Datos del usuario');
+		// Comprobamos si ya existe una dirección de envio para el usuario.
 
 
 
 
+		break;
+
+	case 3:
+		$regMem->setValor('subtitulo', 'Datos bancarios y confirmación');
+		break;
+
+
+	default:
+		$regMem->setValor('subtitulo', 'Paso 1: Comprobar el pedido');
+		break;
+}
 
 include 'cabecera.php';
 ?>
@@ -89,6 +180,7 @@ include 'sidebar-categorias.php';
 
 <div id="main-content">
 	<h2><?=$regMem->getValor('titulo')?></h2>
+	<h3><?=$regMem->getValor('subtitulo')?></h3>
 	<div class="separacion">
 		<?php
 		if ($regError->getError()) {
@@ -114,7 +206,6 @@ include 'sidebar-categorias.php';
 		//Paso 1
 		if ($regMem->getValor('paso')==1 OR !$regMem->getValor('paso')) {
 
-			echo '<h3>Paso 1: Comprobar el pedido</h3>';
 			?>
 			<form action="<?=$_SERVER['SCRIPT_NAME']?>" method="POST">
 				<table>
@@ -132,7 +223,8 @@ include 'sidebar-categorias.php';
 				// Comprobamos si existe el carrito
 				if ($carrito) {
 					// Calculamos la cantidad de productos, y el precio total
-					$total_cantidad = 0;
+					// En esta tabla mostramos los precios sin IVA y el total desglosado al final
+
 					$total_precio = 0;
 					foreach ($carrito as $clave => $linea) {
 						echo "<tr>";
@@ -146,19 +238,36 @@ include 'sidebar-categorias.php';
 						echo "<input type=\"text\" size=\"3\" name=\"cantidad[$clave]\" value=\"{$linea['cantidad']}\"/></td>";
 						echo "</td>";
 
-						echo "<td>" . number_format($linea['precio']) . "&euro;</td>";
-						echo "<td>" . number_format($linea['precio']*$linea['cantidad']) . "&euro;</td>";
+						echo "<td>" . number_format($linea['precio']/(1+IVA),2) . "&euro;</td>";
+						echo "<td>" . number_format($linea['precio']*$linea['cantidad']/(1+IVA),2) . "&euro;</td>";
 						echo "<td>";
 
 						echo "<a href=\"{$_SERVER['SCRIPT_NAME']}?accion=Eliminar&id={$clave}\" title=\"Eliminar del carrito\"><img src=\"images/icon_delete.gif\"/></a>";
 						echo "</td>";
 						echo "</tr>";
+						$total_precio += $linea['precio']*$linea['cantidad'];
 					}
+
 				} else {
 
 				}
 
 				?>
+				<tr>
+					<th>IVA <?=(IVA*100)?>%</th>
+					<th></th>
+					<th></th>
+					<th><?=number_format($total_precio*IVA,2)?>&euro;</th>
+					<th></th>
+				</tr>
+				<tr>
+					<th>TOTAL</th>
+					<th></th>
+					<th></th>
+					<th><?=number_format($total_precio,2)?>&euro;</th>
+					<th></th>
+				</tr>
+
 				</table>
 				<p class="centrado">
 					<input type="submit" name="accion" value="Guardar cambios" />
@@ -176,7 +285,45 @@ include 'sidebar-categorias.php';
 
 
 	<?php
-		}
+		/* 	PASO 2		 */
+		} else if ($regMem->getValor('paso')==2) {
+		?>
+
+		
+		<p class="centrado">Son necesarios los datos de usuario para poder tramitar el pedido.</p>
+		<form class="separacion" action="<?=$_SERVER['SCRIPT_NAME']?>" method="POST">
+			<label>Nombre:</label>
+			<input type="text" name="nombre" value="<?=$regMem->getValor('nombre')?>"/>
+
+			<label>Apellido:</label>
+			<input type="apellido" name="apellido" value="<?=$regMem->getValor('apellido')?>"/>
+
+			<label>Dirección:</label>
+			<input type="text" name="direccion" value="<?=$regMem->getValor('direccion')?>"/>
+
+			<label>Población:</label>
+			<input type="text" name="poblacion" value="<?=$regMem->getValor('poblacion')?>"/>
+
+			<label>CP:</label>
+			<input type="text" name="cp" value="<?=$regMem->getValor('cp')?>"/>
+
+			<input type="hidden" name="paso" value="2" />
+
+
+			<p class="separacion"></p>
+			<input class="siguiente" type="submit" name="accion" value="Continuar" />
+			<p class="volver"><a href="<?=$_SERVER['SCRIPT_NAME']?>?paso=1">Volver</a></p>
+
+
+		</form>
+
+
+
+
+	<?php
+		/* PASO 3  		*/
+		} 
+	
 	?>
 
 
